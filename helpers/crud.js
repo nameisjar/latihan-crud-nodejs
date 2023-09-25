d4f055927bde01c9bd388786884bf3822b1dc546
“nameisjar” <nthia4@gmail.com>
2023-09-25 21:37:16 +0700

    initialisasi commit

---
+++ helpers/crud.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const posts = require('../database/posts');
+const PostModel = require('../models/post');
+
+function create(title, body) {
+    let newPost = new PostModel(posts.id++, title, body);
+    posts.data.push(newPost);
+
+    fs.writeFileSync('./database/posts.json', JSON.stringify(posts, null, 4));
+}
+
+function index() {
+    let posts = JSON.parse(fs.readFileSync('./database/posts.json'));
+    posts.data.forEach((post) => {
+        console.log("nomor = " + post.id, ",title = " + post.title,",konten = " + post.body);
+    })
+ }
+function show(id) { }
+function update(id, title, body) { }
+function destroy(id) { }
+
+module.exports = { create, index, show, update, destroy };
---
+++ index.js
@@ -0,0 +1,10 @@
+const { create, index, show, update, destroy } = require('./helpers/crud');
+
+// test create post
+create('test title', 'test data');
+
+// test show all post
+index();
+// test show detail post by id
+// test update post by id
+// test delete post by id
